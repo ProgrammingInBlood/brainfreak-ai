561ff9034b2d0848c3323a23d428df6c633cd042
Eklavya <eklavyasingh12065@gmail.com>
2023-12-25 06:26:01 +0530

    suggestion fix

--- src/components/Chatbox/ChatboxPlaceholder.tsx
+++ src/components/Chatbox/ChatboxPlaceholder.tsx
@@ -5,61 +5,61 @@ interface Props {
   setInput: (value: string) => void;
 }
 
+const suggestions = [
+  {
+    title: "Create a content calender",
+    subtitle: "for a tiktok account",
+    prompt: "I want to create a content calender for a tiktok account",
+  },
+  {
+    title: "Brainstrom content ideas",
+    subtitle: "for my new podcast or youtube channel",
+    prompt:
+      "I want to brainstorm content ideas for my new podcast or youtube channel",
+  },
+  {
+    title: "Tell me Fun facts",
+    subtitle: "about the universe",
+    prompt: "Tell me fun facts about the universe",
+  },
+  {
+    title: "Write a thank you note",
+    subtitle: "to a good friend",
+    prompt: "Write a thank you note to a good friend",
+  },
+  {
+    title: "Suggest some names",
+    subtitle: "for cafe bar restaurant",
+    prompt: "Suggest some names for cafe bar restaurant",
+  },
+  {
+    title: "Come up with concepts",
+    subtitle: "for a new product",
+    prompt: "Come up with concepts for a new product",
+  },
+  {
+    title: "Write a short story",
+    subtitle: "about a dog and a cat",
+    prompt: "Write a short story about a dog and a cat",
+  },
+
+  {
+    title: "Show me code snippets",
+    subtitle: "for a react app",
+    prompt: "Show me code snippets for a react app",
+  },
+  {
+    title: "Give me ideas ",
+    subtitle: "for a new business, startup or app",
+    prompt: "Give me ideas for a new business, startup or app",
+  },
+];
+
 function ChatboxPlaceholder({ setInput }: Props) {
-  const suggestions = [
-    {
-      title: "Create a content calender",
-      subtitle: "for a tiktok account",
-      prompt: "I want to create a content calender for a tiktok account",
-    },
-    {
-      title: "Brainstrom content ideas",
-      subtitle: "for my new podcast or youtube channel",
-      prompt:
-        "I want to brainstorm content ideas for my new podcast or youtube channel",
-    },
-    {
-      title: "Tell me Fun facts",
-      subtitle: "about the universe",
-      prompt: "Tell me fun facts about the universe",
-    },
-    {
-      title: "Write a thank you note",
-      subtitle: "to a good friend",
-      prompt: "Write a thank you note to a good friend",
-    },
-    {
-      title: "Suggest some names",
-      subtitle: "for cafe bar restaurant",
-      prompt: "Suggest some names for cafe bar restaurant",
-    },
-    {
-      title: "Come up with concepts",
-      subtitle: "for a new product",
-      prompt: "Come up with concepts for a new product",
-    },
-    {
-      title: "Write a short story",
-      subtitle: "about a dog and a cat",
-      prompt: "Write a short story about a dog and a cat",
-    },
-
-    {
-      title: "Show me code snippets",
-      subtitle: "for a react app",
-      prompt: "Show me code snippets for a react app",
-    },
-    {
-      title: "Give me ideas ",
-      subtitle: "for a new business, startup or app",
-      prompt: "Give me ideas for a new business, startup or app",
-    },
-  ];
-
   const fourRandomSuggestions = useMemo(() => {
     const shuffledSuggestions = suggestions.sort(() => 0.5 - Math.random());
     return shuffledSuggestions.slice(0, 4);
-  }, [suggestions]);
+  }, []);
 
   return (
     <div className="w-full">
@@ -71,10 +71,11 @@ function ChatboxPlaceholder({ setInput }: Props) {
       </div>
 
       <div className="grid grid-cols-2 gap-4 p-4">
-        {fourRandomSuggestions.map((suggestion, index) => (
+        {fourRandomSuggestions.map((suggestion) => (
           <div
             key={suggestion.prompt}
             className={`bg-slate-800 rounded-xl p-4  col-span-2 md:col-span-1`}
+            onClick={() => setInput(suggestion.prompt)}
           >
             <h1 className="text-base md:text-lg">{suggestion.title}</h1>
             <p className="font-light text-gray-400 text-sm md:text-base">
