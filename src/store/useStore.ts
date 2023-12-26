08f696fef782afdd5eb4251ddd86cf275f944833
Eklavya <eklavyasingh12065@gmail.com>
2023-12-26 05:35:40 +0530

    changed localStorage to IndexedDB

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,11 +1,32 @@
 import { create } from "zustand";
 import createChatSlice from "./slices/chat";
-import { persist } from "zustand/middleware";
+import { StateStorage, createJSONStorage, persist } from "zustand/middleware";
+import localforage from "localforage";
 
-//add persist middleware
+const localforageInstance = localforage.createInstance({
+  driver: localforage.INDEXEDDB,
+  name: "chat-storage",
+  storeName: "chat-storage",
+  version: 1,
+  size: 104857600,
+});
+
+const storage: StateStorage = {
+  getItem: async (name: string): Promise<string | null> => {
+    return await localforageInstance.getItem(name);
+  },
+  setItem: async (name: string, value: string): Promise<void> => {
+    await localforageInstance.setItem(name, value);
+  },
+  removeItem: async (name: string): Promise<void> => {
+    await localforageInstance.removeItem(name);
+  },
+};
+
 const useStore = create(
   persist(createChatSlice, {
     name: "chat-storage",
+    storage: createJSONStorage(() => storage),
   })
 );
 
