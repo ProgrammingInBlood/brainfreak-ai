c299f8a73f09e8f010ee74e6e968e3474ba2a6b7
Eklavya <eklavyasingh12065@gmail.com>
2023-12-26 05:48:58 +0530

    optimizations and fixed warnings

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -8,17 +8,20 @@ const localforageInstance = localforage.createInstance({
   name: "chat-storage",
   storeName: "chat-storage",
   version: 1,
-  size: 104857600,
+  size: 4980736,
 });
 
 const storage: StateStorage = {
   getItem: async (name: string): Promise<string | null> => {
+    console.log(name, "has been retrieved");
     return await localforageInstance.getItem(name);
   },
   setItem: async (name: string, value: string): Promise<void> => {
+    console.log(name, "with value", value, "has been saved");
     await localforageInstance.setItem(name, value);
   },
   removeItem: async (name: string): Promise<void> => {
+    console.log(name, "has been deleted");
     await localforageInstance.removeItem(name);
   },
 };
